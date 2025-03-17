2e67d755d65cab43f0257e6b95640e233b91cda3
Elio Molas <139929580+ElioMolasDev@users.noreply.github.com>
2025-03-17 12:51:38 -0300

    Time: 8 ms (40.3%), Space: 45.3 MB (5.18%) - LeetHub

---
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+          Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+        for (int count : frequencyMap.values()) {
+            if (count % 2 != 0) {
+                return false; 
+            }
+        }
+
+        return true;
+    }
+}
